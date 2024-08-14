0f26f04f0d12285ca760386d883b9d7b9edc7ad9
xu03xu03 <166630213+xu03xu03@users.noreply.github.com>
2024-08-15 00:25:34 +0800

    Add files via upload

---
+++ stripts.js
@@ -0,0 +1,10 @@
+//平滑滾動效果
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
